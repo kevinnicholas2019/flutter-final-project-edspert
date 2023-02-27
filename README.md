f396f0a8287d3a8d0bb261c7d8f1436c424fece3
Kevin Tan <kevinnicholas2019@gmail.com>
2023-02-27 17:51:31 +0700

    init assets dan utils

--- README.md
+++ README.md
@@ -15,3 +15,21 @@ For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
 "# flutter-final-project-edspert" 
+
+1. Firebase Authentication
+Sign-In with Google
+2. Figma
+    Inspect UI Design
+    Export Image, Illustrator, Icons
+    Final Project URL: 
+        https://www.figma.com/file/vht1hANiGM1sFld1zidoCG/Bootcamp-Flutter-Edspert?node-id=412%3A2
+3. Postman
+    REST API Documentation
+    Inspect Request & Response
+    Postman URL:
+        https://documenter.getpostman.com/view/17757271/2s847FwEJx 
+4. Github
+    https://github.com/Edspert/Bootcamp-Flutter/tree/materi/final-project 
+    https://github.com/public-apis/public-apis
+
+
---
+++ lib/presentation/utils/text_style_app.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class TextStyleApp {
+  //Large text
+  static TextStyle get largeTextDefault => const TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 30,
+        fontWeight: FontWeight.w700,
+        height: 42,
+        letterSpacing: 0,
+      );
+  static TextStyle get largeText30 => largeTextDefault;
+  static TextStyle get largeText24 =>
+      largeTextDefault.copyWith(fontSize: 24, height: 33.6);
+  static TextStyle get largeText20 =>
+      largeTextDefault.copyWith(fontSize: 20, height: 28);
+
+  //Title
+  TextStyle get titleDefault => const TextStyle(
+        fontFamily: 'SF Pro Display',
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+        height: 28,
+        letterSpacing: 0,
+      );
+  TextStyle get titleDefault20 => titleDefault;
+  TextStyle get titleDefault18 =>
+      titleDefault.copyWith(fontSize: 18, height: 25.2);
+  TextStyle get titleDefault16 =>
+      titleDefault.copyWith(fontSize: 16, height: 22.4);
+  TextStyle get titleDefault14 =>
+      titleDefault.copyWith(fontSize: 14, height: 19.6);
+  TextStyle get titleDefault13 =>
+      titleDefault.copyWith(fontSize: 13, height: 18.2);
+  TextStyle get titleDefault10 =>
+      titleDefault.copyWith(fontSize: 10, height: 14);
+  TextStyle get titleDefault8 =>
+      titleDefault.copyWith(fontSize: 8, height: 11.2);
+
+  //Subtitle
+  TextStyle get subtitleDefault => titleDefault.copyWith(
+      fontSize: 16, fontWeight: FontWeight.w500, height: 22.4);
+  TextStyle get subtitle16 => subtitleDefault;
+  TextStyle get subtitle14 =>
+      subtitleDefault.copyWith(fontSize: 14, height: 19.6);
+  TextStyle get subtitle12 =>
+      subtitleDefault.copyWith(fontSize: 12, height: 16.8);
+  TextStyle get subtitle10 =>
+      subtitleDefault.copyWith(fontSize: 10, height: 14);
+  TextStyle get subtitle8 =>
+      subtitleDefault.copyWith(fontSize: 8, height: 11.2);
+}

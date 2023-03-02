87d72549c706e245522ffa164f4ab6eed72693c5
Kevin Tan <kevinnicholas2019@gmail.com>
2023-03-02 14:55:03 +0700

    kasih shadow di nav bar bosq

--- lib/presentation/pages/home/widget/bottom_navigation_bar_app.dart
+++ lib/presentation/pages/home/widget/bottom_navigation_bar_app.dart
@@ -5,100 +5,131 @@ import 'package:flutter/material.dart';
 const _radius = 20.0;
 
 class BottonNavigationBarApp extends StatelessWidget {
+  static final _clipper = _CustomClipPath();
+
   const BottonNavigationBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ClipPath(
-      clipper: CustomClipPath(),
-      child: Container(
-        height: 80,
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                height: 55,
-                decoration: BoxDecoration(
-                  color: ColorsApp.offWhite,
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      blurRadius: 20,
-                      spreadRadius: 1,
-                      offset: const Offset(0, 2),
+    return CustomPaint(
+      painter: _ClipShadowShadowPainter(
+        clipper: _clipper,
+        shadow: Shadow(
+          color: Colors.black.withOpacity(0.15),
+          blurRadius: 10,
+          offset: const Offset(0, 2),
+        ),
+      ),
+      child: ClipPath(
+        clipper: _clipper,
+        child: Container(
+          height: 80,
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: ColorsApp.offWhite,
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        blurRadius: 20,
+                        spreadRadius: 1,
+                        offset: const Offset(0, 2),
+                      ),
+                    ],
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(_radius),
+                      topRight: Radius.circular(_radius),
                     ),
-                  ],
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(_radius),
-                    topRight: Radius.circular(_radius),
                   ),
-                ),
-                child: Row(
-                  children: [
-                    for (var entry in {
-                      "Home": "assets/icons/nav_toggle_on/home.png",
-                      "Diskusi Soal":
-                          "assets/icons/nav_toggle_off/diskusi_soal.png",
-                      "Profile": "assets/icons/nav_toggle_off/profile.png",
-                    }.entries)
-                      Expanded(
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              if (entry.key != "Diskusi Soal")
-                                Image.asset(
-                                  entry.value,
-                                  width: 20,
-                                  height: 20,
+                  child: Row(
+                    children: [
+                      for (var entry in {
+                        "Home": "assets/icons/nav_toggle_on/home.png",
+                        "Diskusi Soal":
+                            "assets/icons/nav_toggle_off/diskusi_soal.png",
+                        "Profile": "assets/icons/nav_toggle_off/profile.png",
+                      }.entries)
+                        Expanded(
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                if (entry.key != "Diskusi Soal")
+                                  Image.asset(
+                                    entry.value,
+                                    width: 20,
+                                    height: 20,
+                                  ),
+                                if (entry.key == "Diskusi Soal")
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                Text(
+                                  entry.key,
+                                  style: TextStyleApp.largeTextDefault.copyWith(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
-                              if (entry.key == "Diskusi Soal")
-                                const SizedBox(
-                                  height: 20,
-                                ),
-                              Text(
-                                entry.key,
-                                style: TextStyleApp.largeTextDefault.copyWith(
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Align(
-              alignment: Alignment.topCenter,
-              child: SizedBox(
-                width: 50,
-                height: 50,
-                child: FloatingActionButton.large(
-                  onPressed: () {},
-                  child: const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: ImageIcon(
-                      AssetImage("assets/icons/Quiz icon.png"),
-                      color: ColorsApp.offWhite,
+              Align(
+                alignment: Alignment.topCenter,
+                child: SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: FloatingActionButton.large(
+                    onPressed: () {},
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: ImageIcon(
+                        AssetImage("assets/icons/Quiz icon.png"),
+                        color: ColorsApp.offWhite,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class CustomClipPath extends CustomClipper<Path> {
+class _ClipShadowShadowPainter extends CustomPainter {
+  final Shadow shadow;
+  final CustomClipper<Path> clipper;
+
+  _ClipShadowShadowPainter({required this.shadow, required this.clipper});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = shadow.toPaint();
+    var clipPath = clipper.getClip(size).shift(shadow.offset);
+    canvas.drawPath(clipPath, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class _CustomClipPath extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var radius = _radius;
